7cfe9d9960782990a8a635c40bc553a4d33c739c
Simon Petrac <pfoerdie@gmail.com>
2023-08-08 13:45:17 +0200

    change from resource.ontology to resource.ontology.core

--- ontology.ids.test.js
+++ ontology.ids.test.js
@@ -3,7 +3,7 @@ const
     expect           = require('expect'),
     {loadDataFiles}  = require('@nrd/fua.module.rdf'),
     {Dataset}        = require('@nrd/fua.module.persistence'),
-    res_data         = require('@nrd/fua.resource.ontology.ids');
+    res_data         = require('./load.ontology.ids.js');
 
 describe('resource.ontology.ids', function () {
 
